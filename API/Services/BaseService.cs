78fbeee63bd729965ba63a586f17d49c95a4e379
Rodrigo Leão <rodrigo.leao@radixeng.com.br>
2023-10-27 13:57:37 -0300

    Create project and first method of database connection

---
+++ API/Services/BaseService.cs
@@ -0,0 +1,17 @@
+using System.Data.SqlClient;
+
+namespace backend {
+    public class BaseService {
+        private static string connectionString()
+        {
+            return "Server=tcp:projeto-danilo-server.database.windows.net,1433;Initial Catalog=projeto-danilo-db;Persist Security Info=False;User ID=rodrigo;Password=#RODehopoder97;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        }
+
+        public static SqlConnection getConnection(){
+            var sqlConn = new SqlConnection(connectionString());
+            sqlConn.Open();
+
+            return sqlConn;
+        }
+    }
+}
